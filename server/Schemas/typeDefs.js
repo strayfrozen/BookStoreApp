9442b11ff0be5c79d6940518acdaf652f4e2c9b2
Stray <strayfrozen@yahoo.com>
2022-03-12 13:55:07 -0600

    updated book type def

--- server/Schemas/typeDefs.js
+++ server/Schemas/typeDefs.js
@@ -8,6 +8,14 @@ type User {
   bookCount: Int
   savedBooks: [Book]        
 }
+type Book {
+  bookId: String
+  authors: [String]
+  description: String
+  title: String
+  image: String
+  link: String
+}
 `;
 
 
