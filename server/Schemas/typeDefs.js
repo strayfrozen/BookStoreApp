5b4c0e47f69c23a63e8fd6936b093d559c287ed6
Stray <strayfrozen@yahoo.com>
2022-03-12 14:26:12 -0600

    added type query me and mutation to type def

--- server/Schemas/typeDefs.js
+++ server/Schemas/typeDefs.js
@@ -20,6 +20,15 @@ type Auth {
   token: ID!
   user: User
 }
+type Query {
+  me: User
+}
+type Mutation {
+  login(email: String!, password: String!): Auth
+  addUser(username: String!, email: String!, password: String!): Auth
+  saveBook(input: SavedBookInput): User
+  removeBook(bookId: String!): User
+}
 `;
 
 
