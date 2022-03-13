13cc54474823d8d9e678900d14e06cbae925f0c5
Stray <strayfrozen@yahoo.com>
2022-03-13 14:14:21 -0500

    set up basic mutation resolver structure

--- server/Schemas/resolvers.js
+++ server/Schemas/resolvers.js
@@ -6,7 +6,25 @@ const resolvers = {
       me:  () => {
         return me.find().sort({ createdAt: -1 });
           }
-     }  
+     }  ,
+     Mutation: {
+      login: async () => {
+  
+      },
+      addUser: async () => {
+  
+      },
+      saveBook: async () => {
+  
+      },
+      removeBook: async () => {
+  
+      }
+    }
+
+
   };
+
+  
   
   module.exports = resolvers;
