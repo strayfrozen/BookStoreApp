5365b3a2e384c4d882ea718c06d1a4302c503b0a
Stray <strayfrozen@yahoo.com>
2022-03-13 14:45:12 -0500

    updated the login mutation resolver

--- server/Schemas/resolvers.js
+++ server/Schemas/resolvers.js
@@ -1,6 +1,6 @@
 //const { signToken } = require('../utils/auth');
 const { User } = require('../models');
-//const { AuthenticationError } = require("apollo-server-express");
+const { AuthenticationError } = require("apollo-server-express");
 
 const resolvers = {
     Query: {
@@ -9,8 +9,20 @@ const resolvers = {
           }
      }  ,
      Mutation: {
-      login: async () => {
-  
+      login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+      
+        if (!user) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+      
+        const correctPw = await user.isCorrectPassword(password);
+      
+        if (!correctPw) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+      
+        return user;
       },
       addUser: async (parent, args) => {
         const user = await User.create(args);
